3a69f829c4fede7cf707b109e1bfd25f4597d0b7
nstarlike <nstar0512@gmail.com>
2024-10-21 22:38:30 +0900

    Write Config interface

--- src/main/java/com/javalogger/config/Config.java
+++ src/main/java/com/javalogger/config/Config.java
@@ -2,4 +2,7 @@ package com.javalogger.config;
 
 public interface Config {
 	public void loadProperty(String propertyName);
+	public void loadProperty();
+	public String getProperty(String key);
+	public void setProperty(String key, String value);
 }
